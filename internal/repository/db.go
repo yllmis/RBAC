9feabd1956e6432ef6ce91b0aa1af6c290fe9723
yllmis <2943246638@qq.com>
2025-12-11 19:06:56 +0800

    新建项目，连接数据库

---
+++ internal/repository/db.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Conn *gorm.DB
+
+func NewMysql() {
+	fmt.Println("正在连接数据库......")
+	my := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"root",
+		"258369",
+		"10.148.126.209",
+		3306,
+		"rabc")
+	conn, err := gorm.Open(mysql.Open(my), &gorm.Config{})
+	if err != nil {
+		fmt.Println("数据库连接失败,请检查参数:", err)
+		panic(err)
+	}
+	Conn := conn
+	fmt.Println("数据库连接成功!")
+
+}
